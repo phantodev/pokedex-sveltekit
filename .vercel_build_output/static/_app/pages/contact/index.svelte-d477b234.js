f3a5edf9e61d64e53ad565d8134888601666fd4c
phantoxe <eduardo@dotmidia.com.br>
2021-11-15 23:10:51 -0300

    add vercel adapter

---
+++ .vercel_build_output/static/_app/pages/contact/index.svelte-d477b234.js
@@ -0,0 +1,15 @@
+import{S as Z,i as $,s as ee,k as S,e as s,t as p,K as te,d as a,n as I,c as r,a as b,g as u,b as i,G as se,f as d,D as t,E as O}from"../../chunks/vendor-efc230f4.js";function ae(Y){let _,v,R,E,n,w,H,y,P,x,q,A,B,l,D,C,G,N,T,j,z,K,L,M,g,h,m,Q,f,V;return{c(){_=S(),v=s("h1"),R=p("Hire my Services"),E=S(),n=s("p"),w=p("Hi, it's me again! "),H=s("br"),y=s("span"),P=p("Eduardo Burko"),x=s("span"),q=s("br"),A=p("a.k.a phanto dev"),B=S(),l=s("p"),D=p("If you need any web development related services please contact me, let's work together :-) "),C=s("br"),G=s("br"),N=p(`\r
+\r
+	I have great experience in development using the best technologies on the market, but at the\r
+	moment my passion is Svelte as you can see!\r
+	`),T=s("br"),j=s("br"),z=p(`\r
+	I hope your contact as soon as possible!`),K=s("br"),L=s("br"),M=p(`\r
+\r
+	Thank you very much!`),g=S(),h=s("div"),m=s("img"),f=s("a"),V=p("gtihub.com/phantodev"),this.h()},l(e){te('[data-svelte="svelte-1ftb4zl"]',document.head).forEach(a),_=I(e),v=r(e,"H1",{class:!0});var U=b(v);R=u(U,"Hire my Services"),U.forEach(a),E=I(e),n=r(e,"P",{class:!0});var k=b(n);w=u(k,"Hi, it's me again! "),H=r(k,"BR",{}),y=r(k,"SPAN",{class:!0});var W=b(y);P=u(W,"Eduardo Burko"),W.forEach(a),x=r(k,"SPAN",{});var F=b(x);q=r(F,"BR",{}),A=u(F,"a.k.a phanto dev"),F.forEach(a),k.forEach(a),B=I(e),l=r(e,"P",{});var o=b(l);D=u(o,"If you need any web development related services please contact me, let's work together :-) "),C=r(o,"BR",{}),G=r(o,"BR",{}),N=u(o,`\r
+\r
+	I have great experience in development using the best technologies on the market, but at the\r
+	moment my passion is Svelte as you can see!\r
+	`),T=r(o,"BR",{}),j=r(o,"BR",{}),z=u(o,`\r
+	I hope your contact as soon as possible!`),K=r(o,"BR",{}),L=r(o,"BR",{}),M=u(o,`\r
+\r
+	Thank you very much!`),o.forEach(a),g=I(e),h=r(e,"DIV",{class:!0});var J=b(h);m=r(J,"IMG",{src:!0,width:!0,height:!0,alt:!0}),f=r(J,"A",{href:!0,class:!0});var X=b(f);V=u(X,"gtihub.com/phantodev"),X.forEach(a),J.forEach(a),this.h()},h(){document.title="Hire Svelte Developer",i(v,"class","text-4xl text-center my-8 uppercase font-bold"),i(y,"class","text-4xl font-bold"),i(n,"class","mb-5"),se(m.src,Q="/giticon.png")||i(m,"src",Q),i(m,"width","40px"),i(m,"height","25px"),i(m,"alt","Logo Pokedex"),i(f,"href","httsp://github.com.br/phantodev"),i(f,"class","ml-2 text-2xl font-bold"),i(h,"class","w-full flex my-5")},m(e,c){d(e,_,c),d(e,v,c),t(v,R),d(e,E,c),d(e,n,c),t(n,w),t(n,H),t(n,y),t(y,P),t(n,x),t(x,q),t(x,A),d(e,B,c),d(e,l,c),t(l,D),t(l,C),t(l,G),t(l,N),t(l,T),t(l,j),t(l,z),t(l,K),t(l,L),t(l,M),d(e,g,c),d(e,h,c),t(h,m),t(h,f),t(f,V)},p:O,i:O,o:O,d(e){e&&a(_),e&&a(v),e&&a(E),e&&a(n),e&&a(B),e&&a(l),e&&a(g),e&&a(h)}}}class le extends Z{constructor(_){super();$(this,_,null,ae,ee,{})}}export{le as default};
